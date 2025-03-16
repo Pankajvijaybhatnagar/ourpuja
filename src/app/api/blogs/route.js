a9e337a03da58941f0d086f0a03fbcfb5620d537
ankitjnvynr <ankitbkana@outlook.com>
2025-03-16 18:37:45 +0530

    tryin

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -4,6 +4,6 @@ export async function GET(request){
     return NextResponse.json({message:"hii it is the blogs response it is the get request",requestData:request})
 }
 
-export async function POST(request){
-    
+export async function POST(request,response){
+
 }
